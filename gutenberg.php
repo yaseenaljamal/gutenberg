1f35a554d4b860c915e317990a4ab41b3a5afd38
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2022-01-21 14:28:18 -0500

    Removed happy new year

--- gutenberg.php
+++ gutenberg.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Gutenberg - Nightly (Happy New Year!)
+ * Plugin Name: Gutenberg - Nightly
  * Plugin URI: https://github.com/bph/gutenberg
  * Description: Zip from trunk (the default branch) of WordPress Gutenberg GitHub repo: Gutenberg This is the development plugin for the new block editor in core. If you have trouble seeing updates, go to <em>Git Updater > Settings</em>  and use the button <em>Refresh Cache</em>. Ping @bph on WPSlack for questions. Or <a href="https://github.com/bph/gutenberg/discussions/new">post on the Discussion board</a>.
 
