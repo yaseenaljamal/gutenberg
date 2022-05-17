4dfa586bcbb3df2d17162dbdc69441c888071983
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2022-05-17 14:03:34 +0100

    verson bump

--- gutenberg.php
+++ gutenberg.php
@@ -6,7 +6,7 @@
 
  * Requires at least: 5.8
  * Requires PHP: 5.6
- * Version: 13.3.20220514
+ * Version: 13.3.20220517
  * Author: Gutenberg Team and Birgit Pauli-Haack (Gutenberg Times)
  * Text Domain: gutenberg
  * GitHub Plugin URI: bph/gutenberg
