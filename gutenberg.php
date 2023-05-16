8df7aa801299cfc2547a314463383157be71ca4e
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2023-05-16 14:13:19 +0200

    version bump

--- gutenberg.php
+++ gutenberg.php
@@ -6,7 +6,7 @@
 
  * Requires at least: 6.1
  * Requires PHP: 5.6
- * Version: 15.9.20230515
+ * Version: 15.9.20230516
  * Author: Gutenberg Team and Birgit Pauli-Haack (Gutenberg Times)
  * Text Domain: gutenberg
  * GitHub Plugin URI: bph/gutenberg
